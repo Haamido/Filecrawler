68d35b86f4f7ae345af194fc683e6e4ed61f7b66
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-09-18 11:51:17 +0200

    add scanner that reads input stream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Ask the user for input string
+        var scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // If a file in the directory, print absolute path to the console
+        
     }
 }
